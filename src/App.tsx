6249440fb5c0fa6d33b2acd4b6478b4684780004
idimnsuhoridem <idimnsuhoridem640@gmail.com>
2025-05-19 09:13:41 -0400

    first react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello";
+
+//Simple react program that outputs a welcome message
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.tsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h3>Hi there, my name's Gideon. Welcome!</h3>;
+}
+
+export default Hello;
